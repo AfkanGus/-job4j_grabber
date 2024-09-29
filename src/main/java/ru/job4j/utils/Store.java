2b169930e758eee4ab59196fb97b87d0365f33b4
afkan_guseynov <af.kan@bk.ru>
2024-09-29 21:01:18 +0500

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359].

--- src/main/java/ru/job4j/utils/Store.java
+++ src/main/java/ru/job4j/utils/Store.java
@@ -5,10 +5,12 @@ import java.util.List;
 /**
  * 3. Архитектура проекта - Аргегатор Java Вакансий [#260359].
  */
-public interface Store {
+public interface Store extends AutoCloseable {
     void save(Post post);
 
     List<Post> getAll();
 
     Post findById(int id);
+
+    void close() throws Exception;
 }
